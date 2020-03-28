cc3b6e3502647a53f1f0e35a9e1e188aff9a6388
rizkyrajitha <rajithagunathilake@gmail.com>
2020-03-28 22:56:38 +0530

    first commit

---
+++ precache-manifest.a3c621be4efdb4f527b93d484558a2cf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "1e9a867753dffa277bae",
+    "url": "/static/js/main.1e9a8677.chunk.js"
+  },
+  {
+    "revision": "8c69745585b3f543db46",
+    "url": "/static/js/2.8c697455.chunk.js"
+  },
+  {
+    "revision": "1e9a867753dffa277bae",
+    "url": "/static/css/main.73211374.chunk.css"
+  },
+  {
+    "revision": "612606ddd1016f8d02785dd7a0d8a212",
+    "url": "/index.html"
+  }
+];
